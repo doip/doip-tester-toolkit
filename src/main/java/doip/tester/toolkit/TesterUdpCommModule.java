5c8eb5a61abcb023765aac7a4c320c1f35ec3ecd
Marco Wehnert <marco.wehnert@gmx.de>
2023-12-17 16:20:41 +0000

    Added method TesterUdpCommModule.sendDoipUdpStatusRequet and added marker to log messages for method entry and exit.

--- src/main/java/doip/tester/toolkit/TesterUdpCommModule.java
+++ src/main/java/doip/tester/toolkit/TesterUdpCommModule.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 
+import doip.library.message.DoipUdpEntityStatusRequest;
 import doip.library.message.DoipUdpVehicleIdentRequest;
 import doip.library.message.DoipUdpVehicleIdentRequestWithEid;
 import doip.library.message.DoipUdpVehicleIdentRequestWithVin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
 public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollection {
 	
@@ -16,6 +19,13 @@ public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollectio
 	
 	private TestConfig config = null;
 
+	/** Log4j marker for function entry */
+	private static Marker enter = MarkerManager.getMarker("ENTER");
+	
+	/** Log4j marker for function exit */
+	private static Marker exit = MarkerManager.getMarker("EXIT");
+	
+	
 	public TesterUdpCommModule(TestConfig config) {
 		super(config);
 		this.config = config;
@@ -33,10 +43,10 @@ public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollectio
 	 *                     to send the data
 	 */
 	public void send(byte[] data, InetAddress address) throws IOException {
-		logger.trace(">>> public void send(byte[] data, InetAddress address)");
+		logger.trace(enter, ">>> public void send(byte[] data, InetAddress address)");
 		this.sendDatagramPacket(data, data.length,
 				address, config.getTargetPort());
-		logger.trace("<<< public void send(byte[] data, InetAddress address)");
+		logger.trace(exit, "<<< public void send(byte[] data, InetAddress address)");
 	}
 
 	/**
@@ -48,12 +58,12 @@ public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollectio
 	 *                     to send the data.
 	 */
 	public void sendDoipUdpVehicleIdentRequest(InetAddress address) throws IOException {
-		logger.trace(">>> public void sendDoipUdpVehicleIdentRequest(InetAddress address) throws IOException");
+		logger.trace(enter, ">>> public void sendDoipUdpVehicleIdentRequest(InetAddress address)");
 		DoipUdpVehicleIdentRequest request = new DoipUdpVehicleIdentRequest();
 		logger.info(TextBuilder.sendMessage(request.getMessageName()));
 		this.send(request, address,
 				config.getTargetPort());
-		logger.trace("<<< public void sendDoipUdpVehicleIdentRequest(InetAddress address) throws IOException");
+		logger.trace(exit, "<<< public void sendDoipUdpVehicleIdentRequest(InetAddress address)");
 	}
 
 	/**
@@ -67,12 +77,12 @@ public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollectio
 	 *                     to send the data.
 	 */
 	public void sendDoipUdpVehicleIdentRequestWithEid(byte[] eid, InetAddress address) throws IOException {
-		logger.trace(">>> public void sendDoipUdpVehicleIdentRequestWithEid(byte[] eid, InetAddress address) throws IOException");
+		logger.trace(enter, ">>> public void sendDoipUdpVehicleIdentRequestWithEid(byte[] eid, InetAddress address) throws IOException");
 		DoipUdpVehicleIdentRequestWithEid request = new DoipUdpVehicleIdentRequestWithEid(
 				eid);
 		this.send(request, address,
 				config.getTargetPort());
-		logger.trace("<<< public void sendDoipUdpVehicleIdentRequestWithEid(byte[] eid, InetAddress address) throws IOException");
+		logger.trace(exit, "<<< public void sendDoipUdpVehicleIdentRequestWithEid(byte[] eid, InetAddress address) throws IOException");
 	}
 
 	/**
@@ -86,47 +96,59 @@ public class TesterUdpCommModule extends DoipUdpMessageHandlerWithEventCollectio
 	 *                     to send the data.
 	 */
 	public void sendDoipUdpVehicleIdentRequestWithVin(byte[] vin, InetAddress address) throws IOException {
-		logger.trace(">>> public void sendDoipUdpVehicleIdentRequestWithVin(byte[] vin, InetAddress address)");
+		logger.trace(enter, ">>> public void sendDoipUdpVehicleIdentRequestWithVin(byte[] vin, InetAddress address)");
 		try {
 			DoipUdpVehicleIdentRequestWithVin request = new DoipUdpVehicleIdentRequestWithVin(
 					vin);
 			this.send(request, address,
 					config.getTargetPort());
 		} finally {
-			logger.trace("<<< public void sendDoipUdpVehicleIdentRequestWithVin(byte[] vin, InetAddress address)");
+			logger.trace(exit, "<<< public void sendDoipUdpVehicleIdentRequestWithVin(byte[] vin, InetAddress address)");
 		}
 	} 
 	
+	public void sendDoipUdpEntityStatusRequest(InetAddress address) throws IOException {
+		String method = "public void sendDoipEntityStatusRequest()";
+		try {
+			logger.trace(enter, ">>> {}", method);
+			DoipUdpEntityStatusRequest request = new DoipUdpEntityStatusRequest();
+			this.send(request, address, config.getTargetPort());
+					
+		} finally {
+			logger.trace(exit, "<<< {}", method);
+		}
+	}
+	
 	@Override
 	public void onHeaderTooShort(DatagramPacket packet) {
-		logger.trace(">>> public void onHeaderTooShort(DatagramPacket packet)");
+		logger.trace(enter, ">>> public void onHeaderTooShort(DatagramPacket packet)");
 		logger.info("Received UDP message which was too short, but there is nothing to do " +
 				"because a diagnostic tester shall not send a negative acknowledge message.");
-		logger.trace("<<< public void onHeaderTooShort(DatagramPacket packet)");
+		logger.trace(exit, "<<< public void onHeaderTooShort(DatagramPacket packet)");
 	}
 	
 	@Override
 	public void onInvalidPayloadLength(DatagramPacket packet) {
-		logger.trace(">>> public void onInvalidPayloadLength(DatagramPacket packet)");
+		logger.trace(enter, ">>> public void onInvalidPayloadLength(DatagramPacket packet)");
 		logger.info("Received UPD message with invalid payload length, but there is nothing to do " +
 				"because a diagnostic tester shall not send a negative acknowledge message.");
-		logger.trace("<<< public void onInvalidPayloadLength(DatagramPacket packet)");
+		logger.trace(exit, "<<< public void onInvalidPayloadLength(DatagramPacket packet)");
 	}
 
 	@Override
 	public void onInvalidPayloadType(DatagramPacket packet) {
-		logger.trace(">>> public void onInvalidPayloadType(DatagramPacket packet)");
+		logger.trace(enter, ">>> public void onInvalidPayloadType(DatagramPacket packet)");
 		logger.info("Received UPD message with invalid payload type, but there is nothing to do " +
 				"because a diagnostic tester shall not send a negative acknowledge message.");
-		logger.trace("<<< public void onInvalidPayloadType(DatagramPacket packet)");
+		logger.trace(exit, "<<< public void onInvalidPayloadType(DatagramPacket packet)");
 	}
 	
 	@Override
 	public void onHeaderIncorrectPatternFormat(DatagramPacket packet) {
-		logger.trace(">>> 	public void onHeaderIncorrectPatternFormat(DatagramPacket packet)");
+		logger.trace(enter, ">>> 	public void onHeaderIncorrectPatternFormat(DatagramPacket packet)");
 		logger.info("Received UPD message with incorrect pattern format, but there is nothing to do " +
 				"because a diagnostic tester shall not send a negative acknowledge message.");
-		logger.trace("<<< 	public void onHeaderIncorrectPatternFormat(DatagramPacket packet)");
+		logger.trace(exit, "<<< 	public void onHeaderIncorrectPatternFormat(DatagramPacket packet)");
 	}
 	
 }
